fa1fecad365db9cd12bc41803c6f24aa73670cf7
PC-20161111EWFU\Administrator <1084063237@qq.com>
2017-04-15 11:37:32 +0800

    第三次

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.print("你哄哄");
+        System.out.print("电风扇要说的话");
     }
 }
